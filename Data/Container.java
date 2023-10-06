8cfbf268b3f53a0146137ad93282406a129c89f5
Dave Sarkies <s3664099@student.rmit.edu.au>
2023-10-07 09:06:45 +1100

    Further developed container with open/close & lock/unlock. Also add item

--- Data/Container.java
+++ Data/Container.java
@@ -12,9 +12,22 @@ import java.util.ArrayList;
 public class Container extends ImmoveableItem implements Item {
 	
 	private ArrayList<CarriableItem> contents;
+	private boolean lockable = false;
+	private boolean closeable = false;
+	private boolean locked = false;
+	private boolean closed = false;
 	
 	public Container(String name, String description) {
 		super(name,description);
+	}
+	
+	public Container(String name, String description, boolean closeable, boolean lockable,
+					boolean locked, boolean closed) {
+		super(name, description);
+		this.closeable = closeable;
+		this.lockable = lockable;
+		this.locked = locked;
+		this.closed = closed;
 	}
 
 	@Override
@@ -29,26 +42,42 @@ public class Container extends ImmoveableItem implements Item {
 	}
 	
 	public String getItemList() {
-		String response = "";
-		response = response.format("The %s contains ",super.getDescription());
-		int length = 0;
+		String response = super.getDescription();
 		
-		for (Item content:contents) {
+		if (!closed) {
+			response = response.format("%s. The %s contains ",super.getDescription(),super.getName());
+			int length = 0;
+		
+			for (Item content:contents) {
 			
-			if (length == 0) {
-				response = response.format("%s %s",response,content.getDescription());
-			} else {
-				response = response.format("%s, %s",response,content.getDescription());
+				if (length == 0) {
+					response = response.format("%s %s",response,content.getDescription());
+				} else {
+					response = response.format("%s, %s",response,content.getDescription());
+				}
+				length ++;
 			}
-			length ++;
-		}
 		
-		if (length == 0) {
-			response = response.format("%s nothing",response);
-		}
+			if (length == 0) {
+				response = response.format("%s nothing",response);
+			}
 		
+		} else {
+			response = response.format("%s. The %s is closed",response, super.getName());
+		}
+			
 		return response;
 	}
+	
+	public void addItem(CarriableItem item) {
+		
+		contents.add(item);
+	}
+	
+	//Check Contents
+	//Remove item
+	//Lock/Unlock
+	//Open/Close
 
 }
 
