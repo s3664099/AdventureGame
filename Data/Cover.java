a7eeff7f7b2d9f541c78b6501700f91c353d7145
Dave Sarkies <s3664099@student.rmit.edu.au>
2024-01-28 14:06:09 +1100

    Continues with the cover class

--- Data/Cover.java
+++ Data/Cover.java
@@ -15,14 +15,32 @@ public class Cover extends ImmoveableItem implements Item,Serializable {
 	ArrayList<Exit> hiddenExits;
 	ArrayList<Item> hiddenItems;
 	private boolean isCover = true;
+	private boolean removeObject;
 	
-	public Cover(String name, String description) {
+	//Adds a hidden exit to the list
+	public Cover(String name, String description, Exit hiddenExit, boolean remove) {
 		super(name,description);
+		this.hiddenExits.add(hiddenExit);
+		this.removeObject = remove;
+	}
+	
+	//Adds a hidden item to the list
+	public Cover(String name, String description, Item hiddenItem, boolean remove) {
+		super(name,description);
+		this.hiddenItems.add(hiddenItem);
+		this.removeObject = remove;
+	}
+	
+	//Adds both hidden exits and items
+	public Cover(String name, String description, Exit hiddenExit, Item hiddenItem, boolean remove) {
+		super(name,description);
+		this.hiddenExits.add(hiddenExit);
+		this.hiddenItems.add(hiddenItem);
+		this.removeObject = remove;
 	}
 
 	@Override
 	public String[] getNouns() {
-		
 		return super.getNouns();
 	}
 
@@ -34,35 +52,29 @@ public class Cover extends ImmoveableItem implements Item,Serializable {
 	//Methods for items that are closeable and lockable
 	@Override
 	public boolean getCloseable() {
-
 		return false;
 	}
 
 	@Override
 	public boolean getClosed() {
-		
 		return false;
 	}
 
 	@Override
 	public boolean getLockable() {
-		
 		return false;
 	}
 
 	@Override
 	public boolean getLocked() {
-		
 		return false;
 	}
 
 	@Override
-	public void setClosed() {		
-	}
+	public void setClosed() {}
 
 	@Override
-	public void setLocked() {		
-	}
+	public void setLocked() {}
 
 	@Override
 	public boolean checkKey(Item key) {
@@ -71,33 +83,54 @@ public class Cover extends ImmoveableItem implements Item,Serializable {
 
 	@Override
 	public boolean getMoveable() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public Exit getHiddenExit() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	//Checks if there are any hidden exits of items
+	//True: check for items
+	//False: check for exits
+	public boolean checkHidden (boolean whatItems) {
+		
+		boolean anyItems = false;
+		
+		if (whatItems) {
+			if (this.hiddenItems.size()>0) {
+				anyItems = true;
+			}
+		} else {
+			if (this.hiddenExits.size()>0) {
+				anyItems = true;
+			}
+		}
+		
+		return anyItems;
+		
+	}
+	
+	//Checks to see if the item is to be removed if there are no more items/exits
+	public boolean checkRemove() {
+		return this.removeObject;
+	}
+	
+	//Removes a hidden item. Sends null if no item available
 	@Override
 	public Item getHiddenItem() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	//Removes a hidden exits. Sends null if no exit available
 	@Override
 	public boolean getMoved() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public void setMoved() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void setMoved() {}
 
 }
 
