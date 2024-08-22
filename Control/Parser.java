eb2fa11938836f95c600e17b8cd00c7bc2f45485
Dave Sarkies <s3664099@student.rmit.edu.au>
2024-08-22 18:55:58 +1000

    Finished examine a move command (for movement)

--- Control/Parser.java
+++ Control/Parser.java
@@ -11,53 +11,37 @@ package Control;
 
 public class Parser {
 	
-	//Go through
-	//Pick up/put down
-	//strip the
-	//String the verb and other words from the main parser so that all that is left
-	//		is the object
-	//Add full object name
-	
 	String[] cardinals = {"north","south","east","west","up","down"};
 	String parsedCommands[] = {"","",""}; 
 	
 	//Turns the command into verb noun formation
 	public String[] parseCommand(String command) {
 		
-		System.out.println(command);
 		String[] commands = command.split(" ");
 		command = command.substring(commands[0].length(), command.length()).trim();
-		System.out.println(command);
-		
-		//First and second words automatically added
-		parsedCommands[0] = commands[0];
 		
 		//If the word move being used instead of go?
 		if (commands[0].equals("move") && checkCardinals(commands)) {
 			commands[0] = "go";
-			parsedCommands[0] = "go";
+		} else if (commands[0].equals("move") && commands.length>2) {
+			commands[0] = "go";
 		}
 		
 		//Checks number of words in command
 		if(commands.length>0) {
-
 			//Checks the cardinal directions. Accepts and removes 'to the'
 			if (commands[0].equals("go") && checkCardinals(commands)) {			
 				if (commands.length>2 && commands[1].equals("to") 
 									  && commands[2].equals("the")) {
 					command = command.substring(7,command.length());
-					System.out.println(command);
 				}
 			
 			//Go command and not a cardinal direction
 			} else if (commands.length>1 && commands[0].equals("go") 
 										 && commands[1].equals("through")) {
 				command = command.substring(8,command.length());
-				System.out.println(command);
 			}
 		}
-		
-		System.out.println(command);
 		
 		//Handles look command
 		if (commands[0].equals("look") && commands.length>0) {
@@ -68,8 +52,13 @@ public class Parser {
 			} else if (command.length() >3 && commands[1].equals("at")) {
 				command = command.substring(3,command.length());
 			}
-			System.out.println(command);
+			
+		//Examine command
+		} else if (commands[0].equals("examine") && commands.length>0) {
+			commands[0] = "look";
 		}
+		
+		//Pick up/put down.
 
 		//Strips 'the' from the object
 		if (command.length()>4 && command.substring(0,4).equals("the ")) {
@@ -77,6 +66,7 @@ public class Parser {
 			System.out.println(command);
 		}
 		
+		parsedCommands[0] = commands[0];
 		parsedCommands[1] = command;
 				
 		return parsedCommands;
